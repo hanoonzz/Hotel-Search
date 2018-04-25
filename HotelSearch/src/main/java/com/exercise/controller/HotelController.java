aabe7abbfe0b4f727a86f8b785bf0cedc324355d
Haneen Aburajouh <h.aburajouh@hotmail.com>
2018-04-25 23:34:31 +0300

    fix error in HotelController

--- HotelSearch/src/main/java/com/exercise/controller/HotelController.java
+++ HotelSearch/src/main/java/com/exercise/controller/HotelController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.exercise.model.HotelFilter;
-import com.pluralsight.model.Goal;
 
 @Controller
 public class HotelController {
